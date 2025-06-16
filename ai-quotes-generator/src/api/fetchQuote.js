5f2796d9d82e60e8211067d477eb0c56746d75f5
Vedant Kankate <98166840+Vedant2402@users.noreply.github.com>
2025-06-15 19:50:49 -0700

    Update

--- ai-quotes-generator/src/api/fetchQuote.js
+++ ai-quotes-generator/src/api/fetchQuote.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchQuote = async () => {
-  const response = await axios.get('https://api.quotable.io/random?tags=&author=');
+  const response = await axios.get('https://api.quotable.io/random');
   return response.data;
 };
